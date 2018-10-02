ffc7346f52d92e62ba1e876e85933ae4148cf633
Jared Hoberock <jaredhoberock@gmail.com>
2018-10-02 14:16:05 -0500

    Refactor single_sender to inherit from basic_sender

--- graph_executor.hpp
+++ graph_executor.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <grid_index.hpp>
+#include <basic_sender.hpp>
 #include <stdexcept>
 
 
@@ -15,72 +15,67 @@ __global__ void single_kernel(Function f)
 }
 
 
-inline void launch(cudaStream_t stream, cudaGraph_t graph)
-{
-   // instantiate the graph
-   cudaGraphExec_t executable_graph{};
-   if(auto error = cudaGraphInstantiate(&executable_graph, graph, nullptr, nullptr, 0))
-   {
-     throw std::runtime_error("detail::submit: CUDA error after cudaGraphInstantiate: " + std::string(cudaGetErrorString(error)));
-   }
-
-   // launch the graph
-   if(auto error = cudaGraphLaunch(executable_graph, stream))
-   {
-     throw std::runtime_error("detail::submit: CUDA error after cudaGraphLaunch: " + std::string(cudaGetErrorString(error)));
-   }
-
-   // delete the graph instance
-   if(auto error = cudaGraphExecDestroy(executable_graph))
-   {
-     throw std::runtime_error("detail::submit: CUDA error after cudaGraphExecDestroy: " + std::string(cudaGetErrorString(error)));
-   }
-}
-
-
 } // end detail
 
 
-class graph_executor;
+template<class,class> class single_sender;
 
 
-template<class Function, class Sender>
-class single_sender
+class graph_executor
 {
   public:
-    void submit(cudaStream_t stream)
+    graph_executor(cudaStream_t s)
+      : stream_(s)
+    {}
+
+    template<class Function, class Sender>
+    single_sender<Function,Sender> then_execute(Function f, Sender& sender) const;
+
+    cudaStream_t stream() const
     {
-      // create a new graph
-      cudaGraph_t graph = make_graph();
-
-      // launch the graph
-      detail::launch(stream, graph);
-
-      // destroy the graph
-      if(auto error = cudaGraphDestroy(graph))
-      {
-        throw std::runtime_error("CUDA error after cudaGraphDestroy: " + std::string(cudaGetErrorString(error)));
-      }
+      return stream_;
     }
 
   private:
+    cudaStream_t stream_;
+};
+
+
+template<class Function, class Sender>
+class single_sender : private basic_sender<single_sender<Function,Sender>, graph_executor, Function, Sender>
+{
+  private:
+    using super_t = basic_sender<single_sender<Function,Sender>, graph_executor, Function, Sender>;
+
+  public:
+    using super_t::executor;
+    using super_t::submit;
+    using super_t::sync_wait;
+
+  private:
+    using super_t::predecessor;
+    using super_t::function;
+
+    // friend super_t so it can access insert() and downcasts
+    friend super_t;
+
+    // friend graph_executor so it can access the private ctor
     friend class graph_executor;
+
+    // friend single_sender so it can access insert()
     template<class,class> friend class single_sender;
 
-    single_sender(Function f, Sender&& predecessor)
-      : function_(f),
-        predecessor_(std::move(predecessor))
-    {}
+    using super_t::super_t;
 
     // this function transliterates the chain of predecessors into a graph
     cudaGraphNode_t insert(cudaGraph_t g) const
     {
       // insert the predecessor
-      cudaGraphNode_t predecessor_node = predecessor_.insert(g);
+      cudaGraphNode_t predecessor_node = predecessor().insert(g);
 
       // introduce a new kernel node
       cudaGraphNode_t result_node{};
-      void* kernel_params[] = {reinterpret_cast<void*>(const_cast<Function*>(&function_))};
+      void* kernel_params[] = {reinterpret_cast<void*>(const_cast<Function*>(&function()))};
       cudaKernelNodeParams node_params
       {
         reinterpret_cast<void*>(&detail::single_kernel<Function>),
@@ -93,58 +88,16 @@ class single_sender
 
       if(auto error = cudaGraphAddKernelNode(&result_node, g, &predecessor_node, 1, &node_params))
       {
-        throw std::runtime_error("CUDA error after cudaGraphAddKernelNode: " + std::string(cudaGetErrorString(error)));
+        throw std::runtime_error("single_sender::insert: CUDA error after cudaGraphAddKernelNode: " + std::string(cudaGetErrorString(error)));
       }
-
-      // discard the predecessor node
-      // XXX not sure if this is best practice or not
-      //     seems like a leak
 
       return result_node;
     }
-
-    cudaGraph_t make_graph() const
-    {
-      // create a new graph
-      cudaGraph_t graph{};
-      if(auto error = cudaGraphCreate(&graph, 0))
-      {
-        throw std::runtime_error("CUDA error after cudaGraphCreate: " + std::string(cudaGetErrorString(error)));
-      }
-
-      // insert into the graph
-      // discard the returned node
-      // XXX not sure if this is best practice or not
-      //     seems like a leak
-      insert(graph);
-
-      // return the graph
-      return graph;
-    }
-
-    static void invoke_host_function(void* user_data)
-    {
-      // cast the user data into a Function
-      Function* function = reinterpret_cast<Function*>(user_data);
-
-      // invoke the function
-      (*function)();
-
-      // delete function
-      delete function;
-    }
-
-    Function function_;
-    Sender predecessor_;
 };
 
-class graph_executor
+template<class Function, class Sender>
+single_sender<Function,Sender> graph_executor::then_execute(Function f, Sender& sender) const
 {
-  public:
-    template<class Function, class Sender>
-    single_sender<Function,Sender> then_execute(Function f, Sender& sender) const
-    {
-      return {f, std::move(sender)};
-    }
-};
+  return {*this, f, std::move(sender)};
+}
 
--- testing/graph_executor.cu
+++ testing/graph_executor.cu
@@ -5,12 +5,18 @@
 
 int main()
 {
+  cudaStream_t stream{};
+  if(auto error = cudaStreamCreate(&stream))
+  {
+    throw std::runtime_error("CUDA error after cudaStreamCreate(): " + std::string(cudaGetErrorString(error)));
+  }
+
   {
     // test A then B
 
     void_sender start;
 
-    graph_executor ex;
+    graph_executor ex(stream);
 
     auto task_a = ex.then_execute(
       [] __host__ __device__ ()
@@ -28,23 +34,14 @@ int main()
       task_a
     );
 
-    cudaStream_t stream{};
-    if(auto error = cudaStreamCreate(&stream))
-    {
-      throw std::runtime_error("CUDA error after cudaStreamCreate: " + std::string(cudaGetErrorString(error)));
-    }
+    task_b.submit();
 
-    task_b.submit(stream);
+    task_b.sync_wait();
+  }
 
-    if(auto error = cudaStreamSynchronize(stream))
-    {
-      throw std::runtime_error("CUDA error after cudaStreamSynchronize: " + std::string(cudaGetErrorString(error)));
-    }
-
-    if(auto error = cudaStreamDestroy(stream))
-    {
-      throw std::runtime_error("CUDA error after cudaStreamDestroy: " + std::string(cudaGetErrorString(error)));
-    }
+  if(auto error = cudaStreamDestroy(stream))
+  {
+    throw std::runtime_error("CUDA error after cudaStreamDestroy(): " + std::string(cudaGetErrorString(error)));
   }
 
   std::cout << "OK" << std::endl;
